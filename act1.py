fa737bf8cc0b46d7de32894fcd141e17e862a4aa
Joshua Morin <joshmorin417@gmail.com>
2023-02-19 17:10:10 -0600

    Agrega Listado de numeros y caja dinamica Clase 18/02/2023

--- act1.py
+++ act1.py
@@ -1,3 +1,4 @@
+from collections import Counter
 from flask import Flask,render_template,redirect,url_for,request
 import forms
 
@@ -5,15 +6,21 @@ app = Flask(__name__)
 
 @app.route('/',methods=['GET','POST'])
 def cajaDinamica():
+     numeros = []
+     contadores = {}
+     minimo = 0
+     maximo = 0
      numeros_form = forms.NumeroForm(request.form)
      if request.method == 'POST' :
-          print(numeros_form.numero.data)
-          if numeros_form.numero.data is not None:                   
-               for i in range(int(numeros_form.numero.data)):
-                    print(i)
-                    print(request.form.get('num'+str(i)))
-     return render_template('cajadinamica.html',form=numeros_form)
-     
+          if len(request.form) > 1:
+               for i in range(len(request.form)):
+                    num = int(request.form.get('num'+str(i)))
+                    numeros.append(num)                    
+               minimo = min(numeros)
+               maximo = max(numeros)
+               counter = Counter(numeros)
+               contadores = dict(counter)
+     return render_template('cajadinamica.html',form=numeros_form,numeros=numeros,minimo=minimo,maximo=maximo,contadores=contadores)
 
 if __name__ == '__main__':
     app.run(debug=True)
